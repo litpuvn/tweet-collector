e62cf21b4d0a57227031101116f9e91e2a9599a3
Hoang Long <litpuvn@gmail.com>
2017-09-06 08:52:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+1. command line tool to collect tweets data by keywords and date range
---
+++ run.py
@@ -0,0 +1,91 @@
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+import argparse
+import string
+import config
+import json
+import os
+
+
+def get_parser():
+    """Get parser for command line arguments."""
+    parser = argparse.ArgumentParser(description="Tweet Downloader")
+    parser.add_argument("-k",
+                        "--keyword",
+                        dest="keywords",
+                        help="Keywords to filter",
+                        default='-')
+    parser.add_argument("-o",
+                        "--output",
+                        dest="output",
+                        help="Output folder",
+                        default= os.getcwd()
+                        )
+    return parser
+
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_secret)
+    api = tweepy.API(auth)
+    #collecting data
+    searchQuery = '#someHashtag'  # this is what we're searching for
+    maxTweets = 100  # Some arbitrary large number
+    tweetsPerQry = 100  # this is the max the API permits
+    fName = 'tweets.txt'  # We'll store the tweets in a text file.
+
+    # If results from a specific ID onwards are reqd, set since_id to that ID.
+    # else default to no lower limit, go as far back as API allows
+    sinceId = None
+
+    # If results only below a specific ID are, set max_id to that ID.
+    # else default to no upper limit, start from the most recent tweet matching the search query.
+    max_id = -1L
+
+    tweetCount = 0
+    print("Downloading max {0} tweets".format(maxTweets))
+    with open(fName, 'w') as f:
+        while tweetCount < maxTweets:
+            try:
+                if (max_id <= 0):
+                    if (not sinceId):
+                        new_tweets = api.search(q=searchQuery, count=tweetsPerQry)
+                    else:
+                        new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                                since_id=sinceId)
+                else:
+                    if (not sinceId):
+                        new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                                max_id=str(max_id - 1))
+                    else:
+                        new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                                max_id=str(max_id - 1),
+                                                since_id=sinceId)
+                if not new_tweets:
+                    print("No more tweets found")
+                    break
+
+                for tweet in new_tweets:
+                    f.write(json.dump(tweet._json) + '\n')
+
+                tweetCount += len(new_tweets)
+                print("Downloaded {0} tweets".format(tweetCount))
+                sinceId = new_tweets[-1].id
+            except tweepy.TweepError as e:
+                # Just exit if any error
+                print("some error : " + str(e))
+                break
+
+    print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
