34f1c5543b944034ec5eb1e89b1f1da1142a2c32
DaltonK324 <43864016+DaltonK324@users.noreply.github.com>
2019-08-09 18:57:16 -0500

    Update run_live.py

--- run_live.py
+++ run_live.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 import time
 import subprocess
-
+import datetime
+from datetime import date, timedelta
 
 
 if __name__ == '__main__':
 
 
     c = 0
-
-
+    now = datetime.datetime.now()
+    last_time = now.hour
     while(True):
-        print("been running for ",4*c," hours")
+        print("We've been running for " + str(4 * c) + " hours")	
+	print("last_time = " + str(last_time))
+	print("now.hour = " + str(now.hour))
+        now = datetime.datetime.now()
         c = c+1
-        proc=subprocess.Popen("./live.py")
-        time.sleep(60*60*4)
-        proc.terminate()
-
-
+	subprocess.call("python live.py", shell=True)	
+	if (now.hour < last_time):
+		yesterday = (datetime.datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+		yesterday_fstring = str(yesterday[5:7]) + "-" + str(yesterday[8:10]) + "-" + str(yesterday[2:4]) + "_0."
+		subprocess.call("cd data; tar -czf " + yesterday_fstring + "tar.gz " + yesterday_fstring + "json", shell = True)
+		print("File for " + yesterday_fstring + "Successfully compressed.")
+		subprocess.call("cd data; rm " + yesterday_fstring + "json", shell = True)
+		print("Json file for " + yesterday_fstring + "Successfully removed.")
+	last_time = now.hour			
